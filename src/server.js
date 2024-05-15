17d9e7157f41ca7d054a0b7b0e69a77b7a496c93
felipeljj <felipelatchuk@gmail.com>
2024-05-15 16:19:16 -0300

    Novo login + Implementação banco de dados + Pagina de cadastro + Autenticação login + PDF melhorad

--- src/server.js
+++ src/server.js
@@ -8,9 +8,6 @@ app.use(express.static('public'));
 
 //pagina inicial, que é a pagina de login
 app.get('/', (req, res) => {
-<<<<<<< Updated upstream
-  res.sendFile('index.html', { root: './views' });
-=======
   res.sendFile('login.html', {root: './views'})
 });
 
@@ -49,7 +46,6 @@ app.post('/login', (req, res) => {
       res.redirect('/');
     }
   });
->>>>>>> Stashed changes
 });
 
 const PORT = process.env.PORT || 3000;
